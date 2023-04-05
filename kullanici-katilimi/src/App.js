95b3cbc9a01d094e042b654caf0432fc70fa301b
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-04-06 02:38:16 +0300

    görevler tamamlandı

--- kullanici-katilimi/src/App.js
+++ kullanici-katilimi/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Form from "./components/Form";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://reqres.in/api/users")
+      .then(function (response) {
+        setMembers(response.data.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const addMember = (member) => {
+    axios
+      .post("https://reqres.in/api/users", member)
+      .then(function (response) {
+        setMembers([...members, response.data]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Add Member</h1>
+      <Form addMember={addMember} />
+
+      <h2>Members:</h2>
+      <ul>
+        {members.map((member) => (
+          <li key={member.id}>
+            {member.first_name} {member.last_name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
